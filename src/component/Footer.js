93eeb09f146e400fd42ed8f4fa5d661f53abdf8a
oqlaalrefai <2015978054@ses.yu.edu.jo>
2021-08-29 21:59:44 +0300

    branch

---
+++ src/component/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Footer extends React.Component {
+  render() {
+    return (
+     <footer>
+     <h5>oqla alrefai</h5>
+     </footer>
+    )
+  }
+}
+export default Footer;
