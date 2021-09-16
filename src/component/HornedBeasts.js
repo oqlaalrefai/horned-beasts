06ed20394517ec3cedc1be4223418587828fd5d2
oqlaalrefai <2015978054@ses.yu.edu.jo>
2021-09-17 02:57:05 +0300

    lab04

--- src/component/HornedBeasts.js
+++ src/component/HornedBeasts.js
@@ -33,7 +33,7 @@ class HornedBeast extends React.Component {
               <Card.Title>{this.props.title}</Card.Title>
               <Card.Text>{this.props.description}</Card.Text>
               <Card.Text>Horns: {this.props.horns}</Card.Text>
-              <Card.Text>Favorites❤️: {this.state.favorites}</Card.Text>
+              <Card.Text>votes❤️: {this.state.favorites}</Card.Text>
               <Button variant="primary" onClick={this.sendData}>
                 View Details
               </Button>
@@ -44,63 +44,3 @@ class HornedBeast extends React.Component {
     );
   }
 
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-class HornedBeasts extends React.Component {
-
-  render() {
-    console.log('props: ', this.props);
-    return (
-      <div>
-        <h2>{this.props.title}</h2>
-        <p>{this.props.description}</p>
-        <img src={this.props.imageUrl} alt="" />
-        <p>{this.props.keyword}</p>
-        <p>{this.props.horns}</p>
-      </div>
-    )
-  }
-
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            votes: 0
-        }
-    }
-
-    incrementNumberOfPets = () => {
-        this.setState({
-            votes: this.state.votes + 1
-        })
-    }
-    displayModal = () => {
-        this.props.displayModal(this.props.title)
-    }
-
-    render() {
-        return (
-            <div>
-                <Card style={{ width: '18rem' }} onClick={this.displayModal}>
-                    <Card.Img variant="top" src={this.props.imageUrl} onClick={this.incrementNumberOfPets} />
-                    <Card.Body>
-                        <Card.Title>{this.props.title}</Card.Title>
-                        <Card.Text>
-                            votes =  {this.state.votes}❤️
-                        </Card.Text>
-                        <Card.Text>
-
-                            {this.state.numberOFvotes} ❤️
-
-                            {this.props.description}
-
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </div>
-        )
-    }
-
-
-}
-export default HornedBeast;
