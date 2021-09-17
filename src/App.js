fafae2a35eb7651be09346c8282cba3a2100d0fd
oqlaalrefai <72412478+oqlaalrefai@users.noreply.github.com>
2021-09-17 03:28:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,68 @@
 
+
+import "./App.css";
+import Data from "./assets/data.json";
+import HornedBeasts from './component/HornedBeasts';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './component/Header';
+import Main from './component/Main';
+import Footer from './component/Footer';
+import Forms from './component/Form'
+import SelectedBeast from './component/SelectedBeast';
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      show: false,
+      img: '',
+      name: '',
+      desc: '',
+    };
+  }
+  showSelectedBeast = (imgUrl, _name, description) => {
+    this.setState({
+      show: true,
+      img: imgUrl,
+      name: _name,
+      desc: description,
+    });
+  };
+  hideSelectedBeast = () => {
+    this.setState({
+      show: false,
+    });
+  };
+  render() {
+    return (
+
+      <>
+      <div>
+
+      <SelectedBeast/>
+      {data.map((element,index) => {
+          return (
+          <HornedBeasts title={element.title} url={element.image_url} description={element.description} key={index}/>
+          );
+      })}
+    </div>
+
+   
+
+        <Header />
+        <Main dataFile={Data} showModal={this.showSelectedBeast} />
+        <SelectedBeast hideModal={this.hideSelectedBeast} show={this.state.show} img={this.state.img} name={this.state.name} desc={this.state.desc}/>
+        <Footer />
+
+      </>
+    );
+
+
+
+
+  }
+}
+export default App;
