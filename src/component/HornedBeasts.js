2cb5a03227701e18a03b86ef8ea45262d1e121b5
oqlaalrefai <2015978054@ses.yu.edu.jo>
2021-09-17 14:14:43 +0300

    hornedBeast

--- src/component/HornedBeasts.js
+++ src/component/HornedBeasts.js
@@ -2,19 +2,18 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
 class HornedBeast extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      favorites: 0,
+      votes: 0,
     };
   }
-  increaseFavorites = () => {
+  increasevotes = () => {
     this.setState({
-      favorites: this.state.favorites + 1,
+      votes: this.state.votes + 1,
     });
-    console.log(this.state.favorites);
+    console.log(this.state.votes);
   };
   sendData = () => {
     this.props.showModal(this.props.imageURL,this.props.title, this.props.description);
@@ -22,25 +21,25 @@ class HornedBeast extends React.Component {
   render() {
     return (
       <>
-        <Col>
           <Card style={{ width: "18rem" }}>
             <Card.Img
               variant="top"
               src={this.props.imageURL}
-              onClick={this.increaseFavorites}
+              onClick={this.increasevotes}
             />
             <Card.Body>
               <Card.Title>{this.props.title}</Card.Title>
               <Card.Text>{this.props.description}</Card.Text>
               <Card.Text>Horns: {this.props.horns}</Card.Text>
-              <Card.Text>votes❤️: {this.state.favorites}</Card.Text>
+              <Card.Text>votes : {this.state.votes}❤️</Card.Text>
               <Button variant="primary" onClick={this.sendData}>
                 View Details
               </Button>
             </Card.Body>
           </Card>
-        </Col>
+
       </>
     );
   }
-
+}
+export default HornedBeast;
